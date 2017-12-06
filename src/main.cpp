004fb546f8b69aa3fdf6842052b68396bcd9947c
Jonathan Poncy <jonathan.poncy@tactfactory.com>
2017-12-06 16:50:02 +0100

    Add library link

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
+#include <app_chrono/Chrono.h>
+
+// #include <app_chrono>
+// #include "app_chrono/chrono.h"
+
+// #include <chrono/Chrono.h>
+// #include "chrono/Chrono.h"
+
+namespace app{}
+using namespace app;
+
 int main() {
   using namespace std;
+
+  Chrono c(0, 0, 0);
 
   cout << "coucou" << endl;
 
